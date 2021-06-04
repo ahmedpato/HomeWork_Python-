6af68def9ab209870f7b9b20009cb5352c7cca92
Programmer <67549087+ahmedpato@users.noreply.github.com>
2021-06-04 19:09:04 +0200

    Add files via upload

---
+++ HomeWork12.py
@@ -0,0 +1,10 @@
+# By : Ahmed Mohammed Ali / 20184020
+number = int(input("Enter Your Number :"))
+
+cube = 0
+
+for i in range(1,number+1):
+    
+    cube = i*i*i
+
+    print("Number is :",i,"and cube of the",i,"is:",cube)
